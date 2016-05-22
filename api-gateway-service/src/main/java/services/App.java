9f98893a2621313af4e58894a0f37211e1efabb5
mescanef <zone@mescanef.net>
2016-05-22 17:17:29 +0200

    Added three microservices in initial version
    Added the following microservices:
    - api-gateway-service
    - config-service
    - discovery-service

---
+++ api-gateway-service/src/main/java/services/App.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+
+@SpringBootApplication
+@EnableSidecar
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
