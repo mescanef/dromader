6fdabebcf2e971e2545c1fbfa2d77bc51bbf8a1b
mescanef <zone@mescanef.net>
2016-05-23 00:31:30 +0200

    Fixed api-gateway & discovery service
    Changes in api-gateway and discovery service.
    Removed unused, commented out, stuff from pom.xml.
    Use localhost as a hostname, within services, for now.

--- discovery-service/src/main/java/services/App.java
+++ discovery-service/src/main/java/services/App.java
@@ -1,15 +1,11 @@
 package services;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@Configuration
-@EnableAutoConfiguration
-@EnableDiscoveryClient
-@EnableConfigServer
+@SpringBootApplication
+@EnableEurekaServer
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
